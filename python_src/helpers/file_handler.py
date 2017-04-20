ae84133960adf27bc4bac6d85e2a14c2a9bc801b
Geir Kulia <geir@kulia.no>
2017-04-20 16:15:08 +0200

    Make function to load all characters and store in a variable

--- python_src/helpers/file_handler.py
+++ python_src/helpers/file_handler.py
@@ -2,6 +2,7 @@ from PIL import Image
 import numpy as np
 import pandas as pd
 
+from os import listdir
 import os
 
 def load_image(path_to_image):
@@ -25,3 +26,15 @@ def save_image_to_csv(image, path_to_image):
 def load_image_from_csv(path_to_image):
 	image_array = pd.read_csv(path_to_image, sep=',', header=None)
 	return np.array(image_array)
+
+def load_all_characters(path_to_database, character):
+	path_to_character = path_to_database + '{}/'.format(character)
+	
+	character_array = np.array([])
+	for filename in listdir(path_to_character):
+		if 0 == character_array.size:
+			character_array = np.array([load_image_from_csv(path_to_character + filename)])
+		else:
+			character_array = np.vstack((character_array, [load_image_from_csv(path_to_character + filename)]))
+		
+	return character_array
