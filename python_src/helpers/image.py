1606851baa6a9f81626969f2ad12bd3c4e76edb3
Geir Kulia <geir@kulia.no>
2017-04-20 14:15:20 +0200

    add function to save image

--- python_src/helpers/image.py
+++ python_src/helpers/image.py
@@ -5,10 +5,17 @@ import numpy as np
 def load_image(path_to_image):
 	return np.array(Image.open(path_to_image))
 
+
+def save_image(image, path_to_image):
+	image = Image.fromarray(image)
+	image.save(path_to_image)
+	
+	
 def show_image(image, colorbar=True):
 	cax = plt.imshow(image, cmap=plt.get_cmap('BrBG'))
 	if colorbar:
-		cbar = plt.colorbar(cax)
+		plt.colorbar(cax)
+		
 		
 def convert_to_sensor_values(image):
-	return image ** 2
+	return image**2
