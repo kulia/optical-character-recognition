b9240d0d3d8dc14827cd0af0082b0e123a8ab923
Geir Kulia <geir@kulia.no>
2017-04-21 13:08:28 +0200

    Make function for loading databse to np.array

--- python_src/helpers/file_handler.py
+++ python_src/helpers/file_handler.py
@@ -5,6 +5,8 @@ import pandas as pd
 from os import listdir
 import os
 
+debug = True
+
 def load_image(path_to_image):
 	return np.array(Image.open(path_to_image))
 
@@ -28,29 +30,23 @@ def save_image_to_csv(image_array, path_to_image):
 	with open(path_to_image + '.csv', 'a+') as f:
 		f.write(format_array_to_csv(image_array))
 		f.write('\n')
-		# np.savetxt(file_handler, '\n', delimiter=',')
 
 
 def format_array_to_csv(array):
 	return str(array)[1:-1].replace(' ', '')
 
 
-def load_image_from_csv(path_to_image):
-	image_array = pd.read_csv(path_to_image, sep=',', header=None)
-	return np.array(image_array)
-
-
-def load_all_characters(path_to_database, character):
-	path_to_character = path_to_database + '{}/'.format(character)
-	
-	character_array = np.array([])
-	
-	for filename in listdir(path_to_character):
-		if 0 == character_array.size:
-			character_array = np.array([load_image_from_csv(path_to_character + filename)])
-		else:
-			character_array = np.vstack((character_array, [load_image_from_csv(path_to_character + filename)]))
-		
-	return character_array
-
-# def load_all_characters_to_panda(path_to_database):
+def load_image_array_from_csv(path_to_image_array):
+	image_array = np.array([])
+	with open(path_to_image_array, 'r') as f:
+		index = 0
+		for line in f.readlines():
+			line = line.strip('\n')
+			line_array = line.split(',')
+			image_array = np.append(image_array, [float(x) for x in line_array])
+			
+			if debug and not index%1000:
+				print('Lines of database loaded: ', index)
+			index += 1
+			
+	return image_array
