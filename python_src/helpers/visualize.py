97a1e1f82ac311a4ac3f689902f22dca1ebde9ec
Geir Kulia <geir@kulia.no>
2017-04-21 17:21:06 +0200

    Make proper plot function for pca

--- python_src/helpers/visualize.py
+++ python_src/helpers/visualize.py
@@ -17,14 +17,26 @@ def image_matrix_to_array(image):
 
 def plot_pca(pca_results, target):
 	number_of_colors = 26
-	colors = [(x * 1.0 / number_of_colors, 0.5, 0.5) for x in range(number_of_colors)]
+	colors = [(x * 1.0 / number_of_colors, 0.5*(np.cos(2*np.pi*x/number_of_colors)+1), 0.5*(np.sin(2*np.pi*x/number_of_colors)+1)) for x in range(number_of_colors)]
 	
 	i = 0
 	
+	print(len(pca_results))
+	print(len(target))
+	
 	for elements in pca_results:
-		plt.scatter(elements[0], elements[1], color=colors[ord(str(target[i])) - 97])
+		plt.scatter(elements[0], elements[1], color=colors[chr_to_index(target[i])])
 		i += 1
 	
 	for i in range(len(colors)):
-		plt.scatter(0, 0, label=chr(i + 97), c=colors[i], alpha=1)
+		plt.scatter(None, None, label=chr(i + 97), c=colors[i], alpha=1)
 		plt.legend()
+		
+def str_to_int(string):
+	string = string.strip('\'')
+	string = ord(string)
+	return string
+
+def chr_to_index(char):
+	char = str_to_int(char)
+	return char-97
