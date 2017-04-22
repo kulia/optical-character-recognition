6d8883087d009e206ec62675a1feaf77226752a2
Geir Kulia <geir@kulia.no>
2017-04-22 15:11:23 +0200

    Add structure for database handling

--- python_src/main.py
+++ python_src/main.py
@@ -17,42 +17,34 @@ if __name__ == '__main__':
 	path = file_handler.Path()
 	path_tmp = file_handler.Path('../database/tmp/')
 	
-	n_sne = 5000
+	n_sne = 1000
 	format_database = True
 	
-	if format_database:
-		# data_augmentation.augment_chars74k_database()
-
+	case_list = ['agument_data', 'original', 'pick_and_save_subset', 'load_subset']
+	# case_list = case_list[0]
+	
+	if 'agument_data' in case_list:
+		data_augmentation.generate_chars74k_csv_database()
+	if 'original' in case_list or 'pick_and_save_subset' in case_list:
 		target = file_handler.load_target_to_array(path.char74k_augmented + 'target.csv')
 		train = file_handler.load_image_array_from_csv(path.char74k_augmented + 'train.csv')
-
-		# train, target, _ = data_augmentation.select_samples(train, target, 4000)
-
-	# 	file_handler.delete_file(path_tmp.char74k_augmented + 'target.csv')
-	# 	file_handler.delete_file(path_tmp.char74k_augmented + 'train.csv')
-	# 	file_handler.save_array_to_csv(target, path_tmp.char74k_augmented + 'target')
-	# 	for line in train:
-	# 		file_handler.save_array_to_csv(line, path_tmp.char74k_augmented + 'train')
-	#
-	# target = file_handler.load_target_to_array(path_tmp.char74k_augmented + 'target.csv')
-	# train = file_handler.load_image_array_from_csv(path_tmp.char74k_augmented + 'train.csv')
-
-	print('Number of samples: ', len(train))
+	if 'pick_and_save_subset' in case_list:
+		train, target, _ = data_augmentation.select_samples(train, target, n_sne)
+		
+		file_handler.delete_file(path_tmp.char74k_augmented + 'target.csv')
+		file_handler.delete_file(path_tmp.char74k_augmented + 'train.csv')
+		
+		file_handler.save_array_to_csv(target, path_tmp.char74k_augmented + 'target')
+		for line in train:
+			file_handler.save_array_to_csv(line, path_tmp.char74k_augmented + 'train')
+	if 'load_subset' in case_list:
+		target = file_handler.load_target_to_array(path_tmp.char74k_augmented + 'target.csv')
+		train = file_handler.load_image_array_from_csv(path_tmp.char74k_augmented + 'train.csv')
 	
-	for i in range(1):
-		i = 9
-		t0 = time.time()
-		print('--- iteration {} ---'.format(i))
-		pca, pca_results = fe.pca_analysis(train[1:], n_components=50)
-		
-		tsne = TSNE(n_components=2, verbose=2, perplexity=2, n_iter=1000, learning_rate=1000, init='pca')
-		tsne_results = tsne.fit_transform(pca_results)
-		
-		plt.figure()
-		image_helpers.plot_pca(tsne_results, target)
-		plt.draw()
-		plt.title('t-SNE: {}'.format(100*(i+1)))
-		print('Timelapce: ', time.time()-t0)
-
+	t0 = time.time()
+	print('--- iteration {} ---'.format(i))
+	pca, pca_results = fe.pca_analysis(train[1:], n_components=50)
+	print('time: ', time.time() - t0)
+	
 	print('Finished')
 	plt.show()
