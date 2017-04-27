325bcf3eedbb57164fe8d7bcff4e697fb0b3fb21
Geir Kulia <geir@kulia.no>
2017-04-27 11:58:36 +0200

    Save figures to latex

--- matlab_src/SVM4OCR.m
+++ matlab_src/SVM4OCR.m
@@ -2,6 +2,10 @@ close all, clear all, clc
 
 set(0,'defaultaxesfontname','times new roman'); % Set default font
 
+FontSize = 24;
+figNum = 0;
+pathToFig = '../report_src/figures/lsvc_matlab/';
+
 %% Data Preparation
 
 fprintf('Preparing dataset... \n')
@@ -12,13 +16,17 @@ imSet = imageSet(Dir,   'recursive');
 % Plot random images
 randImagesN = 8;
 
-figure;
+fig = newFig(fig);
 for i = 1:randImagesN
     subplot(2,(randImagesN/2),i);
     label = randi(length(imSet));
     imshow(imSet(label).ImageLocation{randi(imSet(label).Count)});
     title(imSet(label).Description);
+    ax = gca;
+    ax.FontSize = FontSize;
 end;
+
+saveas(fig, [pathToFig 'random_chars'] , 'epsc')
 
 % Split the dataset into training and test
 [trainingSet, testSet] = partition(imSet, [0.8, 0.2], 'randomized');
@@ -35,18 +43,28 @@ img = read(trainingSet(labl),(randi(trainingSet(labl).Count)));
 [hog_4x4, vis4x4] = extractHOGFeatures(img,'CellSize',[4 4]);
 [hog_8x8, vis8x8] = extractHOGFeatures(img,'CellSize',[8 8]);
 % Show the original image
-figure; 
+fig = newFig(fig); 
 subplot(2,3,1:3); imshow(img);
 % Visualize the HOG features
 subplot(2,3,4);  
 plot(vis2x2); 
 title({'CellSize = [2 2]'; ['Feature length = ' num2str(length(hog_2x2))]});
 subplot(2,3,5);
+ax = gca;
+ax.FontSize = FontSize;
+
 plot(vis4x4); 
 title({'CellSize = [4 4]'; ['Feature length = ' num2str(length(hog_4x4))]});
 subplot(2,3,6);
+ax = gca;
+ax.FontSize = FontSize;
+
 plot(vis8x8); 
 title({'CellSize = [8 8]'; ['Feature length = ' num2str(length(hog_8x8))]});
+ax = gca;
+ax.FontSize = FontSize;
+
+saveas(fig, [pathToFig 'hog_features'] , 'epsc')
 
 % We see that 4-by-4 cell size setting encodes enough spatial information
 % to visually identify a digit shape while limiting the number of
